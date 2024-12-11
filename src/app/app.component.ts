1d09254de290ae22054736e096d2b38ba85bc8d2
Małgorzata Jagieła <malgorzata.jagiela@hotmail.com>
2024-12-11 16:19:00 +0100

    Basic Angular feature discussed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TodoApplication';
+  url = 'https://www.wsiz.edu.pl'
+  message = 'Hello from the component class';
+
+  buttonClicked() {
+    //console.log("button clicked")
+    this.message = 'The message was changed';
+    this.url = 'https://angular.dev'
+  }
+
+  getValue(event: any) {
+    return (event.target as HTMLInputElement).value;
+  }
 }
